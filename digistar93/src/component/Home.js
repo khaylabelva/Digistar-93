a63483deb61511d0a992189d8b57e4944c95d0bf
Khayla Belva Annandira <belvakhayla@gmail.com>
2024-09-02 14:15:12 +0700

    fix navbar and page 1

---
+++ digistar93/src/component/Home.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import QuestionMark from '../assets/QuestionMark.png';
+
+const Home = () => {
+    return (
+      <section className="home">
+        <h1>Solusi Digital Bisnis Logistik</h1>
+        <p>
+          LOGEE hadir sebagai jembatan untuk menghubungkan semua pemain logistik menjadi satu ekosistem yang memberikan solusi dan keuntungan bagi bisnis Anda.
+        </p>
+        <button className="btn-butuh-bantuan">
+          <img src={QuestionMark} alt="Question Mark" className="question-mark" />
+          Butuh Bantuan?
+        </button>
+      </section>
+    );
+  };
+
+export default Home;
+
--- digistar93/src/component/Navbar.js
+++ digistar93/src/component/Navbar.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../App.css';
 import LogoLogee from '../assets/LogoLogee.png';
+import ArrowDown from '../assets/ArrowDown.png';
+import ArrowUp from '../assets/ArrowUp.png';
 
 const Navbar = () => {
+  const [showLanguageOptions, setShowLanguageOptions] = useState(false);
+  const [selectedLanguage, setSelectedLanguage] = useState('ID');
+
+  const toggleLanguageOptions = () => {
+    setShowLanguageOptions(!showLanguageOptions);
+  };
+
+  const handleLanguageChange = (language) => {
+    setSelectedLanguage(language);
+    setShowLanguageOptions(false);
+  };
+
   return (
     <nav className="navbar">
       <div className="logo">
@@ -13,6 +27,31 @@ const Navbar = () => {
         <li><a href="#produk">Produk</a></li>
         <li><a href="#artikel">Artikel</a></li>
         <li><a href="#faq">FAQ</a></li>
+        <li><a href="#hubungi">Hubungi Kami</a></li>
+        <li className="language-dropdown">
+          <button 
+            className={`btn-language ${showLanguageOptions ? 'active' : ''}`} 
+            onClick={toggleLanguageOptions}
+          >
+            {selectedLanguage} <img src={showLanguageOptions ? ArrowUp : ArrowDown} alt="Arrow" className="arrow"/>
+          </button>
+          {showLanguageOptions && (
+            <div className="language-options">
+              <button 
+                className="btn-language-option" 
+                onClick={() => handleLanguageChange('ID')}
+              >
+                ID
+              </button>
+              <button 
+                className="btn-language-option" 
+                onClick={() => handleLanguageChange('EN')}
+              >
+                EN
+              </button>
+            </div>
+          )}
+        </li>
       </ul>
       <div className="nav-buttons">
         <button className="btn-daftar">Daftar</button>
