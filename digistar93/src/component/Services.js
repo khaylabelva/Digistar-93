4bd7385aaed78f0e8537f313d069b4646ed8a2e1
Khayla Belva Annandira <belvakhayla@gmail.com>
2024-09-08 14:18:52 +0700

    add cards di services

--- digistar93/src/component/Services.js
+++ digistar93/src/component/Services.js
@@ -11,15 +11,16 @@ const Services = () => {
         Logee hadir sebagai solusi logistik yang menawarkan efisiensi operasional melalui platform andal, layanan operasi menyeluruh, serta kolaborasi bisnis yang disesuaikan dengan kebutuhan Anda.
       </p>
 
+      <div className="services-card">
       <div className="services-section">
         <h3 className="services-subtitle">Solusi Transportasi</h3>
-        <p className="services-desc">Kemudahan dalam melacak dan mengelola informasi pengiriman barang, penyimpanan stok di gudang, barang dalam perjalanan, hingga tiba barang di lokasi akhir. <span className="read-more">Selengkapnya..</span></p>
+        <p className="services-desc">Ubah cara konvensial menjadi digital. Dengan LOGEE, aktivitas pengiriman oleh pemilik muatan dan pemilik armada jadi lebih cepat, mudah dan ringkas. <span className="read-more">Selengkapnya..</span></p>
         <img src={LogeeTruck} alt="Logee Truck" className="service-image"/>
       </div>
 
       <div className="services-section">
         <h3 className="services-subtitle">Solusi Distribusi</h3>
-        <p className="services-desc">Kemudahan dalam melacak dan mengelola informasi pengiriman barang, penyimpanan stok di gudang, barang dalam perjalanan, hingga tiba barang di lokasi akhir. <span className="read-more">Selengkapnya..</span></p>
+        <p className="services-desc">Solusi lengkap kami untuk alur distribusi dari hulu ke hilir. LOGEE menghubungkan outlet dan distributor dengan jaringan principal yang tersebar di berbagai wilayah Indonesia. <span className="read-more">Selengkapnya..</span></p>
         <img src={LogeePort} alt="Logee Port" className="service-image"/>
       </div>
 
@@ -27,6 +28,7 @@ const Services = () => {
         <h3 className="services-subtitle">Solusi Visibility</h3>
         <p className="services-desc">Kemudahan dalam melacak dan mengelola informasi pengiriman barang, penyimpanan stok di gudang, barang dalam perjalanan, hingga tiba barang di lokasi akhir. <span className="read-more">Selengkapnya..</span></p>
         <img src={LogeeVisibility} alt="Logee Visibility" className="service-image"/>
+      </div>
       </div>
     </div>
   );
