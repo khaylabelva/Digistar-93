2b5a4a6d99cb8a9d3bceef3a420a62e5ead5fc94
Khayla Belva Annandira <belvakhayla@gmail.com>
2024-09-17 11:34:52 +0700

    fix footer sesuai design

--- digistar93/src/component/Footer.js
+++ digistar93/src/component/Footer.js
@@ -158,39 +158,13 @@ const Footer = () => {
         <button className="footer-main-button">Selengkapnya</button>
       </div>
       <div className="footer-bottom">
-        <div className="footer-contact">
-            <img src={LogoLogee} alt="LOGEE logo" />
-            <div className="footer-detail">
-            <p>
-            <img src={EmailIcon} alt="Email Icon" /><a href="mailto:info@logee.id">info@logee.id</a>
-            </p>
-            <p>
-            <img src={PhoneIcon} alt="Phone Icon" /><a href="tel:+628111255250">+62 811 1255 250</a>
-            </p>
-            <p>
-            <img src={AddressIcon} alt="Address Icon" />Gedung Telkom DBT, Jln. Prof. Dr. Soepomo no. 139, Jakarta Selatan, 12810 - Indonesia
-            </p>
-            </div>
-        </div>
-        <div className="footer-solutions">
-            <h3>Solusi</h3>
-            <ul>
-            <li><a href="#">Transportasi</a></li>
-            <li><a href="#">Distribusi</a></li>
-            <li><a href="#">Visibility</a></li>
-            </ul>
-        </div>
-        <div className="footer-links">
-            <h3>Tautan</h3>
-            <ul>
-            <li><a href="#">Leap</a></li>
-            <li><a href="#">Artikel</a></li>
-            <li><a href="#">Karier</a></li>
-            <li><a href="#">Hubungi Kami</a></li>
-            </ul>
-        </div>
+    <div className="footer-logo-section">
+        <img src={LogoLogee} alt="LOGEE logo" className="footer-logo" />
+        <p className="footer-copyright">Hak Cipta &copy; 2022 LOGEE. Seluruh hak dilindungi</p>
         <div className="footer-social">
+            <div className="footer-social-google">
             <a href="https://play.google.com/store/apps/developer?id=logee.id&pli=1"><img src={GooglePlay} alt="Google Play" /></a>
+            </div>
             <a href="https://www.instagram.com/logee.id/"><img src={Instagram} alt="Instagram" /></a>
             <a href="https://www.linkedin.com/company/logee-indonesia/"><img src={LinkedIn} alt="LinkedIn" /></a>
             <a href="https://www.facebook.com/people/Logeeid/100084864591594/"><img src={Facebook} alt="Facebook" /></a>
@@ -198,16 +172,42 @@ const Footer = () => {
             <a href="https://x.com/logee_id"><img src={X} alt="X" /></a>
             </div>
         </div>
+    </div>
+    <div className="footer-links-section">
+        <div className="footer-links">
+            <h3>Tautan</h3>
+            <ul>
+                <li><a href="#">Leap</a></li>
+                <li><a href="#">Artikel</a></li>
+                <li><a href="#">Hubungi Kami</a></li>
+                <li><a href="#">Karier</a></li>
+                <li><a href="#">Syarat & Ketentuan</a></li>
+                <li><a href="#">Kebijakan Privasi</a></li>
+                <li><a href="#">FAQ</a></li>
+            </ul>
         </div>
-
-        <div className="footer-bottom-bar">
-        <div className="footer-copyright">
-            <p>Hak Cipta &copy; 2022 LOGEE. Seluruh hak dilindungi</p>
+        <div className="footer-solutions">
+            <h3>Solusi</h3>
+            <ul>
+                <li><a href="#">Transportasi</a></li>
+                <li><a href="#">Distribusi</a></li>
+                <li><a href="#">Visibility</a></li>
+            </ul>
         </div>
-        <div className="footer-up">
-            <a href="#"><img src={ArrowIcon} alt="Back to top" /></a>
+        <div className="footer-contact">
+            <h3>Kontak</h3>
+            <p>Kantor Pusat</p>
+            <p>Gedung Telkom DBT, Jln. Prof. Dr. Soepomo no. 139, Jakarta Selatan, 12810 - Indonesia</p>
+            <p><img src={EmailIcon} alt="Email Icon" /><a href="mailto:info@logee.id">info@logee.id</a></p>
+            <p><img src={PhoneIcon} alt="Phone Icon" /><a href="tel:+628111255250">+62 811 1255 250</a></p>
+            <p>Direktorat Jenderal Perlindungan Konsumen dan Tertib Niaga Kementrian Perdagangan Republik Indonesia (Ditjen PPKTN)</p>
+            <p>Tel: +62 853 1111 11010</p>
         </div>
-        </div>
+    </div>
+    <div className="footer-up">
+        <a href="#"><img src={ArrowIcon} alt="Back to top" /></a>
+    </div>
+</div>
     </footer>
   );
 };
